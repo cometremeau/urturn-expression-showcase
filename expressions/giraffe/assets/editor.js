3a67544742dff0b07c045cda2b32707056b080f0
Olivier Amblet <olivier.amblet@webdoc.com>
2013-06-14 10:12:16 +0200

    fixed focus

--- expressions/giraffe/assets/editor.js
+++ expressions/giraffe/assets/editor.js
@@ -1,86 +1,140 @@
 UT.Expression.ready(function(post) {
-  var $background = $('.post-background');
-  var $panel = $('.sticker-panel');
+  var $background = $('.post-background'), // background utImage panel selector
+      $panel = $('.sticker-panel'), // utSticker selector
+      state = {
+        hasImage: false, // is an image present?
+        stickerCount: 0 // number of stickers
+      };
+
+  // update the validity state of the post based on its current state.
+  // To be valid, a post must have sticker > 1 and an image
+  var validates = function(){
+    post.valid(state.hasImage && state.stickerCount > 0);
+  };
+
   $background.utImage()
+    // set the post height to the image height
+    // and update the state
     .on('utImage:loaded', function(){
-      post.size($(this).height());
-      stickerManager.enable();
+      state.hasImage = true;
+      // resize the post
+      post.size($(this).height(), function(){
+        // once done, display the sticker manager and revalidate
+        stickerManager.enable();
+        validates();
+      });
     })
+    // keep the actual size and disable the
+    // add sticker button
     .on('utImage:removed', function(){
+      console.log('REMOVED');
+      state.hasImage = false;
       stickerManager.disable();
+      stickerManager.removeAll();
+      validates();
     });
 
+  // Sticker Manager handle the rendering of the stickers and their addition.
   var stickerManager = (function stickerManagerSingleton(){
-    var $addButton = $('.button-add-sticker');
-    var collection = post.storage.stickers || [];
-    var maxZIndex = 10;
-    var stickerCount = 0;
+    var $addButton = $('.button-add-sticker'), // add sticker button
+        collection = post.storage.stickers || []; // holds the data inside each sticker
 
-    var handleAddGiraffeEvent = function(event){
-      var data = {stickerId: UT.uuid(), name: $(event.target).data('giraffe')};
-      drawSticker(data);
+    // Handler for when a sticker should be added to the scene.
+    var handleAddStickerEvent = function(event){
+      // generate a unique uuid that will be associated with the utSticker component
+      var id = UT.uuid();
+      // the data object contains the data to reconstituate the sticker content.
+      var data = {stickerId: id, name: $(event.target).data('giraffe')};
+      drawSticker(data)
+        .utSticker('focus'); // and give it the focus.
+      // Stop event propagation to prevent blur of the just focused element
+      event.preventDefault();
+      event.stopPropagation();
+
+      // Save the data
       collection.push(data);
+      // HACK: we need to re-assigne the collection
+      // because if we don't do that the storage won't
+      // detect there was an update to the array.
       post.storage.stickers = collection;
       post.save();
 
+      // Remove the 'cancel' navigation state
       post.popNavigation();
       closePanel();
     };
 
-    var stickerAdded = function() {
-      stickerCount ++;
-      post.valid(stickerCount > 0);
-    };
-    var stickerRemoved = function() {
-      stickerCount --;
-      post.valid(stickerCount > 0);
-    };
     var drawSticker = function(data){
-      stickerAdded();
-      $('<img class="sticker-giraffe" src="assets/images/giraffe_'+data.name+'.png"/>')
+      // define a width which is about 33% of the post width
+      var width = parseInt(post.size().width / 3, 10);
+      // create and return a new sticker node
+      return $('<img class="sticker-giraffe" src="assets/images/giraffe_'+data.name+'.png"/>')
+        // that is append to the post outter node
         .appendTo(post.node)
+        // make it a sticker with a width of 33% of the post width
         .utSticker({
-          width: 64,
+          id: data.stickerId, // link the sticker to its data using the same generated uuid
+          width: width,
           height: 'auto',
-          top: $background.height()/2 - 48,
-          left: $background.width()/2 - 32,
-          id: data.stickerId
+          top: parseInt($background.height()/2 - (width*1.2)/2, 10), // center vertically
+          left: parseInt($background.width()/2 - width/2, 10) // center horizontally
         })
+        // listen to the image load event (IMG, not utSticker)
         .on('load', function(event){
+          // ensure the sticker is saved with the current image size.
           $(event.target).utSticker('save');
+          // there is one more sticker, officially.
+          state.stickerCount ++;
+          validates(); // we are probably a valid post at this state.
         })
+        // listen to the remove event to invalidate the post and remove
+        // the associated data from collection
         .on('utSticker:remove', function(event){
           $.each(collection, function(idx){
             if(this.stickerId === data.stickerId){
-              collection = collection.splice(idx, 1);
-              console.log('removed from collection');
+              collection.splice(idx, 1);
+              state.stickerCount --;
+              validates();
+              return;
             }
-            stickerRemoved();
           });
+          // HACK: we need to re-assigne the collection
+          // because if we don't do that the storage won't
+          // detect there was an update to the array.
           post.storage.stickers = collection;
           post.save();
         });
     };
+    var enableScene = function(){
+      $('.sticker-giraffe').utSticker('show');
+      $addButton.show();
+    };
 
+    var disableScene = function(){
+      $('.sticker-giraffe').utSticker('hide');
+      $addButton.hide();
+    };
+
+    // Close the panel once a sticker has been selected
+    // or the pop state triggered from outside.
     var closePanel = function(){
       $background.utImage('update', {editable: true});
       $panel.hide();
-      $addButton.show();
-      $('.sticker-giraffe').utSticker('show');
+      enableScene();
     };
 
-    // Display the panel of giraffe and let
+    // display the panel of giraffe and let
     // you choose one.
     var addStickerWorkflow = function(){
       post.pushNavigation('cancel', closePanel);
       $background.utImage('update', {editable: false});
       $panel.show();
-      $addButton.hide();
-      $('.sticker-giraffe').utSticker('hide');
+      disableScene();
     };
 
+    // bind the add button
     $addButton.on('click', addStickerWorkflow);
-    $('.button-giraffe').on('click', handleAddGiraffeEvent);
+    $('.button-giraffe').on('click', handleAddStickerEvent);
 
     $.each(collection, function(){
       console.log('collection item', this);
@@ -88,11 +142,10 @@ UT.Expression.ready(function(post) {
     });
 
     return {
-      enable: function(){
-        $addButton.show();
-      },
-      disable: function(){
-        $addButton.hide();
+      enable: enableScene,
+      disable: disableScene,
+      removeAll: function(){
+        $('.sticker-giraffe').utSticker('remove');
       }
     };
   }());
