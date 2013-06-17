b621a236671c3b1ce42d38ec5f6f1e1b993f1cf6
Emmanuel Vuigner <evuigner@gmail.com>
2013-06-17 20:33:23 +0100

    meme: fix player

--- expressions/meme/js/player.js
+++ expressions/meme/js/player.js
@@ -11,11 +11,20 @@
       });
    $("#header_text")
      .utText({
-
+       placeholder: "Write here",
+       maxFontSize: "72px",
+       minFontSize: 36,
+       fixedSize: true,
+       chars: 60,
+       reuse: true
      });
    $("#footer_text")
      .utText({
-
+       placeholder: "And here...",
+       maxFontSize: 72,
+       minFontSize: 36,
+       fixedSize: true,
+       chars: 60
      });
 
   });
