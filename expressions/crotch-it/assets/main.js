be225bcd363dc515b4d743e5b0d2243b43451cca
Olivier Amblet <olivier@amblet.net>
2013-07-19 15:28:49 +0200

    Added crotch-it, a simple HTML layer based expression.

---
+++ expressions/crotch-it/assets/main.js
@@ -0,0 +1,17 @@
+UT.Expression.ready(function(post) {
+  function resize() {
+    post.size($('.frame').height());
+  }
+
+  function init() {
+    $('.frame-content')
+      .utImage()
+      .on('utImage:change', function(event, newValues, oldValues) {
+        resize();
+        post.valid(!!newValues) // valid if there is a value;
+      });
+    resize();
+  }
+
+  init()
+});
