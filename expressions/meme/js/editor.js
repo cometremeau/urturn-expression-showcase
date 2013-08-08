e8d2dce1f90798d39e92ce73784a0f9ef65f0adb
Alexey Puzenko <alexey82.work@gmail.com>
2013-08-08 16:56:41 +0300

    updated meme for use utImage v1.1.8

--- expressions/meme/js/editor.js
+++ expressions/meme/js/editor.js
@@ -1,4 +1,4 @@
-/*global UT: true, jQuery: true */
+/* global UT:true,jQuery:true */
 
 ;(function(UT, $) {
   "use strict";
@@ -7,78 +7,85 @@
 
     $("#meme")
       .utImage({
-        reuse: true
+        styles: {
+          width: 576
+        }
       })
-      .on('utImage:ready',function() {
-        if (!$(this).utImage('data').url) {
-          $(this).utImage('dialog');
+      .on('utImage:ready',function(event, data) {
+        if(!data.data) {
+          $(this).utImage('dialog', {forceQuit:true});
         }
-        post.size({'height':$(this).outerHeight()});
       })
-      .on('utImage:change',function(event, newValues, oldValues) {
-        if (newValues && newValues.data) {
-          $(post.node).addClass('image-is-present');
-        }
-        if(!newValues.data && oldValues.data) {
-          $(post.node).removeClass('image-is-present');
-        }
-        post.size({'height':$(this).outerHeight()});
+      .on('utImage:resize', function(event, data) {
+        post.size({'height':data.height}, function(){
+          jQuery(":utText").utText('adaptFontSize');
+        });
+      })
+      .on('utImage:mediaReady',function() {
+        $(post.node).addClass('image-is-present');
+        setTimeout(function() {
+          jQuery(":utText").utText('adaptFontSize');
+        }, 50);
+        checkValidContent();
+      })
+      .on('utImage:mediaRemove',function() {
+        $(post.node).removeClass('image-is-present');
         setTimeout(function() {
           jQuery(":utText").utText('adaptFontSize');
         }, 50);
         checkValidContent();
       });
-   $("#header_text")
-     .utText({
-       placeholder: "Write here",
-       maxFontSize: 62,
-       minFontSize: 24,
-       fixedSize: true,
-       chars: 60,
-       reuse: true,
-       tabIndex: 1
-     })
-     .on('utText:save',function() {
+    $("#header_text")
+      .utText({
+        placeholder: "Write here",
+        maxFontSize: 62,
+        minFontSize: 24,
+        fixedSize: true,
+        chars: 60,
+        reuse: true,
+        tabIndex: 1
+      })
+    .on('utText:save',function() {
       checkValidContent();
-     });
-   $("#footer_text")
-     .utText({
-       placeholder: "And here...",
-       maxFontSize: 62,
-       minFontSize: 24,
-       fixedSize: true,
-       chars: 60,
-       reuse: true,
-       tabIndex: 2
-     })
-     .on('utText:save',function() {
-      checkValidContent();
-     });
+    });
+    $("#footer_text")
+      .utText({
+        placeholder: "And here...",
+        maxFontSize: 62,
+        minFontSize: 24,
+        fixedSize: true,
+        chars: 60,
+        reuse: true,
+        tabIndex: 2
+      })
+      .on('utText:save',function() {
+        checkValidContent();
+      });
 
-  function textLength(node){
-    var v = node.html().replace(/<br\s*\/?>/mg,"\n");
-    v = v.replace(/(<([^>]+)>)/ig,'');
-    return $.trim(v.replace(/&nbsp;/ig,'')).length;
-  }
-
-  function indexContent() {
-    var text = '';
-    text += $("#header_text .ut-text-content").text();
-    text += " ";
-    text += $("#footer_text .ut-text-content").text();
-    post.storage.indexedContent = text;
-  }
-
-  function checkValidContent() {
-    // check if image is present, and either one or the other text is filled
-    if ((textLength($("#header_text .ut-text-content")) !== 0 ||
-      textLength($("#footer_text .ut-text-content")) !== 0 ) &&
-      $("#meme").utImage('data')) {
-      post.valid(true);
-      indexContent();
-    } else {
-      post.valid(false);
+    function textLength(node) {
+      var v = node.html().replace(/<br\s*\/?>/mg,"\n");
+      v = v.replace(/(<([^>]+)>)/ig,'');
+      return $.trim(v.replace(/&nbsp;/ig,'')).length;
     }
-  }
+
+    function indexContent() {
+      var text = '';
+      text += $("#header_text .ut-text-content").text();
+      text += " ";
+      text += $("#footer_text .ut-text-content").text();
+      post.storage.indexedContent = text;
+    }
+
+    function checkValidContent() {
+      // check if image is present, and either one or the other text is filled
+      if ((textLength($("#header_text .ut-text-content")) !== 0 ||
+        textLength($("#footer_text .ut-text-content")) !== 0 ) &&
+        $("#meme").utImage('data')) {
+        post.valid(true);
+        indexContent();
+      } else {
+        post.valid(false);
+      }
+    }
   });
 }(UT, jQuery));
--- expressions/meme/js/player.js
+++ expressions/meme/js/player.js
@@ -1,35 +1,30 @@
-/*global UT: true, jQuery: true */
-
+/* global UT:true, jQuery:true */
 ;(function(UT, $) {
   "use strict";
   
   UT.Expression.ready(function(post) {
-
-    if (post.context.player === true) {
-      $(post.node).addClass('image-is-present');
-    }
+    $(post.node).addClass('image-is-present');
 
     $("#meme")
       .utImage()
-      .on('utImage:change', function(){
-        post.size($(this).height());        
+      .on('utImage:resize', function(event, data) {
+        post.size(data.height);
       });
 
-   $("#header_text")
-     .utText({
-       maxFontSize: 72,
-       minFontSize: 24,
-       fixedSize: true,
-       chars: 60,
-       reuse: true
-     });
-   $("#footer_text")
-     .utText({
-       maxFontSize: 72,
-       minFontSize: 24,
-       fixedSize: true,
-       chars: 60
-     });
-
+    $("#header_text")
+      .utText({
+        maxFontSize: 72,
+        minFontSize: 24,
+        fixedSize: true,
+        chars: 60,
+        reuse: true
+      });
+    $("#footer_text")
+      .utText({
+        maxFontSize: 72,
+        minFontSize: 24,
+        fixedSize: true,
+        chars: 60
+      });
   });
 }(UT, jQuery));
