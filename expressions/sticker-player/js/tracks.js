33751bbf3b6f6fe5d87bfb750e6bc70bb40917d5
Dmitry Dudin <dudin@xbsoftware.com>
2013-08-26 12:34:08 +0300

    playlist postcard is sound card now + bugfix

--- expressions/sticker-player/js/tracks.js
+++ expressions/sticker-player/js/tracks.js
@@ -1,39 +1,39 @@
 
 // ONLY ITUNES AND SOUNDCLOUD TRACKS ARE AVAILABLE FOR NOW
 
-/* !!!IMPORTANT!!! 
-   USE ONLY US AND UK STORES ITUNES TRACKS. 
-   OTHER STORES TRACKS WON'T BE AVAILABLE IN ALL COUNTRIES */
+/* !!!IMPORTANT!!!
+   USE ONLY US AND UK STORES ITUNES TRACKS.
+   OTHER STORES TRACKS WON"T BE AVAILABLE IN ALL COUNTRIES */
 
 window.tracksList = [
 /* ----------------------------------------------------------------------------------------- */
 { // Track URL
-  'url': 'https://soundcloud.com/haimtime/haim-falling',
+  "url": "https://soundcloud.com/haimtime/haim-falling",
   // Track name (as it will appear in the expression)
-  'html': 'HAIM - Falling'},
-  
-/* ----------------------------------------------------------------------------------------- */
-{ // Track URL
-  'url': 'https://itunes.apple.com/gb/album/dont-save-me/id575608305?i=575608312',
-  // Track name (as it will appear in the expression)
-  'html': 'HAIM - Don\'t Save Me'},
-  
-/* ----------------------------------------------------------------------------------------- */
-{ // Track URL
-  'url': 'https://soundcloud.com/haimtime/haim-falling-duke-dumont',
-  // Track name (as it will appear in the expression)
-  'html': 'HAIM - Falling (Duke Dumont Remix)'},
+  "html": "HAIM - Falling"},
 
 /* ----------------------------------------------------------------------------------------- */
 { // Track URL
-  'url': 'https://itunes.apple.com/us/album/get-lucky-feat.-pharrell-williams/id617154241?i=617154366',
+  "url": "https://itunes.apple.com/gb/album/dont-save-me/id575608305?i=575608312",
   // Track name (as it will appear in the expression)
-  'html': 'Daft Punk - Get Lucky (feat. Pharrell Williams)'},
+  "html": "HAIM - Don't Save Me"},
 
 /* ----------------------------------------------------------------------------------------- */
 { // Track URL
-  'url': 'https://itunes.apple.com/gb/album/oceans-feat.-frank-ocean/id669458187?i=669458347',
+  "url": "https://soundcloud.com/haimtime/haim-falling-duke-dumont",
   // Track name (as it will appear in the expression)
-  'html': 'Jay Z - Oceans (feat. Frank Ocean)'}
+  "html": "HAIM - Falling (Duke Dumont Remix)"},
+
+/* ----------------------------------------------------------------------------------------- */
+{ // Track URL
+  "url": "https://itunes.apple.com/us/album/get-lucky-feat.-pharrell-williams/id617154241?i=617154366",
+  // Track name (as it will appear in the expression)
+  "html": "Daft Punk - Get Lucky (feat. Pharrell Williams)"},
+
+/* ----------------------------------------------------------------------------------------- */
+{ // Track URL
+  "url": "https://itunes.apple.com/gb/album/oceans-feat.-frank-ocean/id669458187?i=669458347",
+  // Track name (as it will appear in the expression)
+  "html": "Jay Z - Oceans (feat. Frank Ocean)"}
 
 ];
